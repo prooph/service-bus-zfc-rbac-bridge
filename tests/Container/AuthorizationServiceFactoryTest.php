177cd182339d9de963c63d6a23186062f6da1aa7
Bas Kamer <bas@bushbaby.nl>
2016-09-02 14:06:59 +0200

    update test

--- tests/Container/AuthorizationServiceFactoryTest.php
+++ tests/Container/AuthorizationServiceFactoryTest.php
@@ -15,7 +15,10 @@ use Interop\Container\ContainerInterface;
 use PHPUnit_Framework_TestCase as TestCase;
 use Prooph\ServiceBusZfcRbacBridge\Container\ZfcRbacAuthorizationServiceBridgeFactory;
 use Prooph\ServiceBusZfcRbacBridge\ZfcRbacAuthorizationServiceBridge;
+use Prooph\ServiceBusZfcRbacBridge\ZfcRbacV3AuthorizationServiceBridge;
+use ZfcRbac\Service\AuthorizationServiceInterface;
 use ZfcRbac\Service\AuthorizationServiceInterface as ZfcRbacAuthorizationService;
+use Zend\Authentication\AuthenticationServiceInterface;
 
 /**
  * Class AuthorizationServiceFactoryTest
@@ -23,20 +26,54 @@ use ZfcRbac\Service\AuthorizationServiceInterface as ZfcRbacAuthorizationService
  */
 final class AuthorizationServiceFactoryTest extends TestCase
 {
+    public function setUp() {
+        $method     = new \ReflectionMethod(AuthorizationServiceInterface::class, 'isGranted');
+        $num        = $method->getNumberOfParameters();
+        $this->isV2 = 2 === $num;
+        $this->isV3 = 3 === $num;
+    }
+
     /**
      * @test
      */
     public function it_creates_authorization_service()
     {
+        if (!$this->isV2) {
+            $this->markTestSkipped();
+        }
+
         $zfcRbacAuthorizationService = $this->prophesize(ZfcRbacAuthorizationService::class);
 
         $container = $this->prophesize(ContainerInterface::class);
-        $container->get('ZfcRbac\Service\AuthorizationService')->willReturn($zfcRbacAuthorizationService->reveal());
+        $container->get(\ZfcRbac\Service\AuthorizationService::class)->willReturn($zfcRbacAuthorizationService->reveal());
 
         $factory = new ZfcRbacAuthorizationServiceBridgeFactory();
 
         $authorizationService = $factory($container->reveal());
 
         $this->assertInstanceOf(ZfcRbacAuthorizationServiceBridge::class, $authorizationService);
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_authorization_serviceV3()
+    {
+        if (!$this->isV3) {
+            $this->markTestSkipped();
+        }
+
+        $zfcRbacAuthorizationService = $this->prophesize(ZfcRbacAuthorizationService::class);
+        $authenticationService = $this->prophesize(\Zend\Authentication\AuthenticationServiceInterface::class);
+
+        $container = $this->prophesize(ContainerInterface::class);
+        $container->get(\Zend\Authentication\AuthenticationServiceInterface::class)->willReturn($authenticationService->reveal());
+        $container->get(ZfcRbacAuthorizationService::class)->willReturn($zfcRbacAuthorizationService->reveal());
+
+        $factory = new ZfcRbacAuthorizationServiceBridgeFactory();
+
+        $authorizationService = $factory($container->reveal());
+
+        $this->assertInstanceOf(ZfcRbacV3AuthorizationServiceBridge::class, $authorizationService);
     }
 }
